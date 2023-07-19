10613e80e811657141de6212cc112c2aa6e7b25b
Ali Anıl Hazar <112640395+anilhazar@users.noreply.github.com>
2023-07-19 18:43:33 +0300

    Update Robots.java

--- Robots.java
+++ Robots.java
@@ -1,4 +1,4 @@
-// Base abstratc class for the project
+// Base abstract class for the project
 public abstract class Robots implements IFeatures{
     public double score;
     private String name;
